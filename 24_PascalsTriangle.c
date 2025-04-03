bc8af18f3851b5b31743a73b27a6e05ee74524ef
Mohammad Parvez <hasanparvez583@gmail.com>
2025-04-03 15:44:38 +0600

    Update 24_PascalsTriangle.c

--- 24_PascalsTriangle.c
+++ 24_PascalsTriangle.c
@@ -1,6 +1,24 @@
+//Write a c program to Print Pascals Triangle
+
 #include<stdio.h>
 
-int main()
-{
-    int i,j,k;
+int main(){
+int rows,i,j,num;
+printf("Enter the number of rows for Pascal's Triangle: ");
+scanf("%d",&rows);
+
+for(i=0;i<rows;i++) {
+num=1; //First value in each row is always 1
+for (j=0;j<rows-i-1;j++){
+printf("  ");
+}
+for(j=0;j<=i;j++) {
+printf("%d",num);
+num=num*(i-j)/(j+1);
+}
+
+printf("\n");
+}
+
+return 0;
 }
