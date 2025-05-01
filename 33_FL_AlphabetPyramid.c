2c9ea003a8b5b532cddc37a9a290269b209d280e
Mohammad Parvez <hasanparvez583@gmail.com>
2025-05-02 00:19:13 +0600

    Update 33_FL_AlphabetPyramid.c

--- 33_FL_AlphabetPyramid.c
+++ 33_FL_AlphabetPyramid.c
@@ -4,13 +4,38 @@
 
 int main()
 {
-int i,j,k,n;
+char ch='A';
+int i,j,n;
 printf("enter number of rows:");
 scanf("%d",&n);
 
 for(i=1;i<=n;i++)
 {
+    for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
     for(j=1;j<=i;j++)
+    {
+        printf("%c ",64+j);
+    }
 
+    printf("\n");
+}
+
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+    for(j=1;j<=i;j++)
+    {
+        printf("%c ",ch);
+
+    }
+       ch++;
+    printf("\n");
+}
 return 0;
 }
